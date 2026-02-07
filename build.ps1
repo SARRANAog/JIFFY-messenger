8139b8ad5fdb94360e119f91fe0c2d01e9443722
SARRANAog <maks.fomenko.06@gmail.com>
2026-02-07 13:45:44 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ if (!(Test-Path ".venv")) {
 .\.venv\Scripts\python.exe -m pip install --upgrade pip
 .\.venv\Scripts\python.exe -m pip install -r requirements.txt
 
-# генерим ico из web/logo.png
+
 .\.venv\Scripts\python.exe tools\make_icon.py
 
 if (Test-Path "dist") { Remove-Item -Recurse -Force "dist" }
